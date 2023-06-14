e9fa8433f207a0d93a0a5bcbf34d1169866b8c78
webacademy <lauralimaufam@gmail.com>
2023-06-13 20:37:09 -0400

    add departamento.ts

---
+++ src/models/Departamentos.ts
@@ -0,0 +1,41 @@
+import {
+    Table, Model, Column, DataType, IsUUID, PrimaryKey,
+    AllowNull, IsEmail, Unique, ForeignKey, HasOne
+} from "sequelize-typescript";
+
+import { Funcionarios } from
+    "./Funcionarios";
+
+@Table({
+    timestamps: true,
+})
+
+export class Departamentos extends Model {
+    @IsUUID('all')
+    @PrimaryKey
+    @Column({
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV1,
+    })
+    id!: string;
+    @AllowNull(false)
+    @Unique
+    @Column({
+        type: DataType.STRING,
+    })
+    name!: string;
+    @AllowNull(false)
+    @Column({
+        type: DataType.STRING,
+    })
+    sigla!: string;
+    @ForeignKey(() => Funcionarios)
+    @AllowNull(true)
+    @Column({
+        type: DataType.UUID,
+    })
+    gestorId!: string;
+    @HasOne(() => Funcionarios,
+        'gestorId')
+    gestor!: Funcionarios;
+}
